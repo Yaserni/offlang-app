98ebcfeb87beca7bab34d7bf03b3c47a028c2683
Abdulrhamn <abd0525656@gmail.com>
2021-07-01 04:46:52 +0300

    fix

--- app.py
+++ app.py
@@ -128,5 +128,5 @@ def classifyDB():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=int(os.environ.get('PORT',8080)))
+    app.run(port=int(os.environ.get('PORT',8080)))
    #app.run()
