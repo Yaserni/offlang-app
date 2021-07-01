c2517a89f590c6d09e9024d9f177d0434608d086
Abdulrhamn <abd0525656@gmail.com>
2021-07-01 06:32:45 +0300

    add_path

--- app.py
+++ app.py
@@ -1,19 +1,18 @@
 import pandas as pd
 import os
 from flask import Flask, render_template, request
-#import absorbProfile as ap  # abosrbProfile - in order to absort tweets from profile
-#import SVM_model_hebrew as svm_model_he
-#import ReadSheetsFiles as rsf
 import pandas as pd
+#import SVM_model_arabic as SVAR
+#import SVM_model_hebrew as SVHE
 
 app = Flask(__name__)
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
-global arabicBert 
 def clssifydatabase(filepath):
+    result="test"
     print(filepath)
     model_name = request.form['model_name']
-
+    '''
     if model_name == 'SVM for Hebrew':
         offensive_count, non_Offensive = svm_model_he.classify_DB(filepath)
 
@@ -27,22 +26,16 @@ def clssifydatabase(filepath):
     # removing the file from the server
     print('remove file - ', filepath)
     os.remove(filepath)
+    '''
     return render_template('prediction.html', result=result)
 
 
-def make_BERT_Predict(text,lang):
-    print(text)
-    data = pd.DataFrame(text)
-    data.to_csv("text.csv")
-    if lang == 'ar':
-        os.system('python load_arabic_bert.py 1')
-    # os.remove('text.csv')
-    # os.remove('text.csv')
+
 
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('index.html',res=APP_ROOT)
 
 
 @app.route("/home", methods=['GET', 'POST'])
@@ -56,15 +49,12 @@ def ClassifyText():
     tweet_content = request.form['tweet_content']
     
     if model_name == 'SVM for Hebrew':
-        result = svm_model_he.predict(tweet_content)
+        result = SVHE.predict(tweet_content)
         result = 'Not Offensive' if (result == 0) else 'Offensive'
-    
    
-    
     res = 'the tweet ' + str(tweet_content) + ' is ' + str(result)
     return render_template('prediction.html', result=res)
 
-# get tweets from Profile
 
 @ app.route('/classifyProfile', methods=['POST'])
 def classifyProfile():
@@ -79,12 +69,10 @@ def classifyProfile():
     elif model_name == 'BERT for Arabic':
         os.system('python load_arabic_bert.py 2 '+ filepath)
         result_file = pd.read_csv('result.csv')
-        # os.remove('result.csv')
         offensive_count, non_Offensive = result_file['res'].iloc[1], result_file['res'].iloc[0]
 
     result ='The number of neutral = '+str(non_Offensive) + ', the number of Offensive ' + str(offensive_count)
 
-    # removing the file from the server
     print('remove file - ', filepath)
     os.remove(filepath)
     return render_template('prediction.html', result=result)
@@ -111,15 +99,7 @@ def classifyDB():
         print('******************************')
 
         file.save(destination)
-    print(destination)
-    
-    # Rewrite the csv file 
-    # df = rsf.readFileFunction(destination)
-
-    # df.to_excel(destination.split('.')[0]+'.xlsx')   
-    # print("The Rewriting process done") 
-    # print(destination.split('.')[0]+'.xlsx')
-    
+    print(destination)   
     return clssifydatabase(destination) # run the models on the uploaded model
 
 
@@ -129,4 +109,3 @@ def classifyDB():
 
 if __name__ == '__main__':
     app.run()
-   #app.run()
