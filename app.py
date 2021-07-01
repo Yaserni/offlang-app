4267aee21fa6a156baa450df8c613cac909744c8
Yaser <yaser.nmr.yn@gmail.com>
2021-07-02 01:19:50 +0300

    Update app.py

--- app.py
+++ app.py
@@ -117,18 +117,18 @@ global vectorizer_ar
 
 def load_he_svm_model():
     # Load Hebrew - SVM model
-    # global svm_model_he
-    # global vectorizer_he
+    global svm_model_he
+    global vectorizer_he
     
     
-    # svm_model_he = joblib.load("finalized_model_he.sav")
-    # vectorizer_he = joblib.load("vectorizer_he.sav")
+    svm_model_he = joblib.load("finalized_model_he.sav")
+    vectorizer_he = joblib.load("vectorizer_he.sav")
     # Load Arabic - SVM model
     
-    global svm_model_ar
-    global vectorizer_ar
-    svm_model_ar = joblib.load("arabic.sav")
-    vectorizer_ar = joblib.load("arvectorizer.sav")
+    # global svm_model_ar
+    # global vectorizer_ar
+    # svm_model_ar = joblib.load("arabic.sav")
+    # vectorizer_ar = joblib.load("arvectorizer.sav")
     # return model, vectorizer
 
 
