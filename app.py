226e3c366016a3323762d067c350dab096ef1966
Abdulrhamn <abd0525656@gmail.com>
2021-07-02 03:41:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -9,25 +9,21 @@ import SVM_model_hebrew as SVMHE
 app = Flask(__name__)
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+
 def clssifydatabase(filepath):
-    result="test"
+    result = "test"
     print(filepath)
     model_name = request.form['model_name']
-    '''
-    if model_name == 'SVM for Hebrew':
-        offensive_count, non_Offensive = svm_model_he.classify_DB(filepath)
 
-    elif model_name == 'BERT for Arabic':
-        print('in the BERT BERT for Arabic')
-        os.system('python load_arabic_bert.py 2 '+filepath)
-        print('Before reading the result file \n\n\n\n\n')
-        result_file = pd.read_csv('result.csv')
-        offensive_count, non_Offensive = result_file['res'].iloc[1], result_file['res'].iloc[0]
-    result = "The number of neutral = " + str(non_Offensive) + ", the number of Offensive " + str(offensive_count) 
-    # removing the file from the server
-    print('remove file - ', filepath)
+    if model_name == 'Hebrew':
+        offensive_count, non_Offensive = SVMHE.classify_DB(filepath)
+
+    elif model_name == 'Arabic':
+        offensive_count, non_Offensive = SVMAR.classify_DB(filepath)
+
+    result ='The number of neutral = '+str(non_Offensive) + ', the number of Offensive ' + str(offensive_count)
     os.remove(filepath)
-    '''
     return render_template('prediction.html', result=result)
 
 
@@ -87,7 +83,6 @@ def classifyProfile():
 @ app.route('/classifyDB', methods=['POST'])
 def classifyDB():
     target = os.path.join(APP_ROOT, 'uploads\\')
-    print(target)
     if not os.path.isdir(target):
         os.mkdir(target)
     print('*************** After if ***************')
